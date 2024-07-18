773cf95c5b2df4fe6b7126f33f469589bda154e3
Harvey C <harveydavid14@gmail.com>
2024-07-17 21:48:34 -0500

    *Moved CustomConditions.java to Utils package to avoid it getting generated by maven in target location

--- src/test/java/ta/training/page_object_model/page/task1/PasteBinHomePageTask1.java
+++ src/test/java/ta/training/page_object_model/page/task1/PasteBinHomePageTask1.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import ta.training.CustomConditions;
+import ta.training.utilities.CustomConditions;
 import java.time.Duration;
 
 /**
